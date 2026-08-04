dc92581763f67c11aab29c1ccb680190ec61268a
grego <gregovichstella04@gmail.com>
2026-08-04 17:41:15 -0300

    prática 2 - parte 1

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,33 +1,18 @@
 package com.example.weatherapp
 
-import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.weatherapp.ui.pages.HomePage
+import com.example.weatherapp.ui.pages.ListPage
+import com.example.weatherapp.ui.pages.MapPage
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,35 +23,9 @@ class MainActivity : ComponentActivity() {
             WeatherAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     HomePage(modifier = Modifier.padding(innerPadding))
+                    //ListPage(modifier = Modifier.padding(innerPadding))
+                    //MapPage(modifier = Modifier.padding(innerPadding))
                 }
-            }
-        }
-    }
-}
-
-
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalContext.current as Activity
-    Column(
-        modifier = modifier.fillMaxSize().padding(24.dp),
-        horizontalAlignment = CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-
-    ) {
-        val modifier = modifier.fillMaxWidth(fraction = 0.9f)
-        Text(
-            text = "Bem-vindo/a!",
-            fontSize = 24.sp
-        )
-        Row(modifier = modifier.padding(12.dp).fillMaxSize(),
-            horizontalArrangement = Arrangement.Start,
-
-            ) {
-
-            Button( onClick = {activity.finish()}
-            ) {
-                Text("Sair")
             }
         }
     }
